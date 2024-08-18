c93147e4b564178ef3eda66e6ef84067b6190b23
Y3CM <pauljons27@gmail.com>
2024-08-18 09:18:26 -0500

    add get user

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -15,3 +15,20 @@ export const getAllUsers = async(req,res)=>{
             return res.status(500).json({ message: "Error en la consulta" });
     }
 }
+
+
+export const getUser = async(req,res)=>{
+
+    try{
+
+        const { id } = req.params;
+        const [rows] = await pool.query("SELECT * FROM usuarios WHERE num_doc = ?", [id,]);
+
+        res.json(rows);
+        if(rows.length <= 0){
+            return res.status(404).json({message:"usuario no encontrados"});
+        }
+    }catch (error){
+            return res.status(500).json({ message: "Error en la consulta" });
+    }
+}
--- routes/UserRutas.js
+++ routes/UserRutas.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
-import { getAllUsers } from "../models/UsersModel.js";
+import { getAllUsers, getUser } from "../models/UsersModel.js";
 
 const router = Router();
 
 router.get("/users", getAllUsers);
+
+router.get("/users/:id", getUser);
 
 
 export default router;
